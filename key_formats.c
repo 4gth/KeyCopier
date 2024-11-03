fb7282a1010303b45481849e01dff3e3c5f76402
4gth <74095667+4gth@users.noreply.github.com>
2024-11-04 10:06:53 +1100

    Update key_formats.c

--- key_formats.c
+++ key_formats.c
@@ -41,4 +41,42 @@ const KeyFormat all_formats[] = {
     .macs = 7,
     .clearance = 8
     }
+    {
+    .manufacturer = "Lockwood",
+    .format_name = "LW4",
+    .format_link = "",
+    .first_pin_inch = 0.231,
+    .last_pin_inch = 1.012,
+    .pin_increment_inch = 0.1562,
+    .pin_num = 5,
+    .pin_width_inch = 0.031,
+    .elbow_inch = 0.1,
+    .drill_angle = 90, // This should actually be 100 but the current resolution will make 100 degrees very ugly and unsuable
+    .uncut_depth_inch = 0.335,
+    .deepest_depth_inch = 0.2,
+    .depth_step_inch = 0.015,
+    .min_depth_ind = 0,
+    .max_depth_ind = 9,
+    .macs = 7,
+    .clearance = 8
+    }
+    {
+    .manufacturer = "Lockwood",
+    .format_name = "LW5",
+    .format_link = "",
+    .first_pin_inch = 0.231,
+    .last_pin_inch = 1.012,
+    .pin_increment_inch = 0.1562,
+    .pin_num = 6,
+    .pin_width_inch = 0.031,
+    .elbow_inch = 0.1,
+    .drill_angle = 90, // This should actually be 100 but the current resolution will make 100 degrees very ugly and unsuable
+    .uncut_depth_inch = 0.335,
+    .deepest_depth_inch = 0.2,
+    .depth_step_inch = 0.015,
+    .min_depth_ind = 0,
+    .max_depth_ind = 9,
+    .macs = 7,
+    .clearance = 8
+    }
 };
